2dae228ef55c7560ce9a948103e991a87b0eb720
AndrewH72 <andrewhsu72@gmail.com>
2024-11-21 19:49:04 -0800

    created display and populate functions

--- main.cpp
+++ main.cpp
@@ -2,15 +2,44 @@
 #include <random>
 using namespace std;
 
+#define MAX_ROWS 5
+#define MAX_COLS 5
+#define MAX_CELLS 5
+
+void displayUniverse(int, int, int[][MAX_COLS]);
+void populateUniverse(int, int[][MAX_COLS]);
+
 int main(){
-  int numRows = 5, numColumns = 5;
-  int gameBoard[numRows][numColumns];
+  // Variables.
+  int theUniverse[MAX_ROWS][MAX_COLS] = {0};
 
-  for(int i = 0; i < 5; i++){
-    int randomNumber = rand() % 5;
-    cout << randomNumber << endl;
+  // Populating 
+  populateUniverse(MAX_CELLS, theUniverse);
+
+  displayUniverse(MAX_ROWS, MAX_COLS, theUniverse);
+
+}
+
+void displayUniverse(int numRows, int numCols, int theUni[][MAX_COLS]){
+  for(int i = 0; i < numRows; i++){
+    for(int j = 0; j < numCols; j++){
+      cout << theUni[i][j] << " ";
+    }
+    cout << endl;
   }
+}
 
-  
 
+void populateUniverse(int maxCells, int theUni[][MAX_COLS]){
+  srand(time(0));
+  int liveCells = 0;
+  while(liveCells < maxCells){
+    int randomRow = rand() % 5;
+    int randomCol = rand() % 5;
+
+    if(theUni[randomRow][randomCol] != 1){
+      theUni[randomRow][randomCol] = 1;
+      liveCells++;
+    }
+  }
 }
